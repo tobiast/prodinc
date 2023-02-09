c5c47ce0b9a2cf5b4336346d89946a9599f925e8
tobiast <tobiast@gmail.com>
2023-02-10 00:03:30 +0100

    added items

--- app.js
+++ app.js
@@ -2,10 +2,32 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
 
+
+// få ekspress til å dele en public-folder til CSS, bilder osv
+app.use(express.static(__dirname + '/public'));
+
 app.get("/", (req, res) => res.type('html').send(html));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
+
+// bruk pug som template-motor (Skrive HTML)
+app.set('view engine', 'pug');
+
+// side som håndterer item
+var items = require('./items.js');
+app.use('/items', items);
+
+// Side som brukes til å svare på ping: En slags pulssjekk
+app.use('/ping', function (req, res) {
+    res.render("message", { title: 'PONG!', message: 'pong'})
+});
+
+
+// fang alle URL-er som ikke er mappet opp. Svare med en fin feilside
+app.get('*', function(req, res) {
+    res.render("error", { title: 'Siden finnes ikke!', message: 'Du har forsøkt å gå til en side som ikke finnes.'})
+});
 
 const html = `
 <!DOCTYPE html>
---
+++ items.js
@@ -0,0 +1,98 @@
+// oppsett av express
+var express = require('express');
+var router = express.Router();
+
+// funksjon som viser liste av produkter
+router.get('/', async function(req, res, next){
+
+   try{
+      let limit = req.query.limit;
+      let s = req.query.s;
+      let order = req.query.sortOrder;
+
+      let products = await getProducts(0, 100)
+      let searchString; 
+
+      if (s){
+         products = products.filter(product => containsSearchString(product, s))
+         searchString = 'Viser resultater for: ' + s;
+      }
+
+      // products = sort(products, order);
+
+      products = products.slice(0,limit)
+
+      res.render("items", { title: 'Items!',  products: products, searchString: searchString})
+   } catch(err){
+       next(err);
+   }
+});
+
+function sort(products, order){
+
+   console.log("order: "  + order)
+      if (order){
+         switch(order){
+            case "1": {
+                return products.sort((product1, product2) => product1.price - product2.price)
+            }
+            case "2": {
+               return products.sort((product1, product2) => product2.price - product1.price)
+           }
+            case "3": {   
+               return products.sort((product1, product2) => product1.title.localeCompare(product2.title))
+            }
+            case "4": {   
+               return products.sort((product1, product2) => product2.title.localeCompare(product1.title))
+           }
+           case "5": {
+               return products.sort((product1, product2) => product1.discountPercentage - product2.discountPercentage)
+            }
+            case "6": {
+               return products.sort((product1, product2) => product2.discountPercentage - product1.discountPercentage)
+            }
+            case "7": {   
+               return products.sort((product1, product2) => product1.brand.localeCompare(product2.brand))   
+            }
+            case "8": {   
+               return products.sort((product1, product2) => product2.brand.localeCompare(product1.brand))
+            }
+         }
+      }
+
+}
+
+// funksjon som håndterer detaljvisning av et produkt på ID
+router.get('/:id', async function(req, res){
+   let products = await getProducts(0, 100)
+   res.render("item", { title: 'Item!',  product: products[req.params.id-1]})
+});
+
+
+// setter sammmen en tekst av viktige deler av produkt. Sjekker om søkestreng er en del av dem. 
+function containsSearchString(product, s){
+   let fieldsToSearch = product.title + product.desctiption + product.brand
+   return fieldsToSearch.toLowerCase().includes(s.toLowerCase())
+}
+
+// funksjon som henter en liste av produkter. 
+// dummyjson.com støtter søk og limit, men jeg ville gjerne kode det selv
+// så jeg bruker ikke det. 
+async function getProducts(num, page){
+
+   const url = 'https://dummyjson.com/products?skip=' + num + '&limit=' + page 
+    
+return await fetch(url)
+   .then((response) => response.json())
+   .then((json) => json.products)
+   .catch(function (err) {
+      console.log("Unable to fetch -", err);
+      err.type = 'redirect'
+      return err
+   });   
+}
+
+
+
+//Sørg for at rutene er tilgjengelig for app.js
+module.exports = router;
